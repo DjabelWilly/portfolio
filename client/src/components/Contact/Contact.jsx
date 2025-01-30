85db4b74b5a9b2119f78c3ba550c1e40b6eae2a1
DjabelWilly <djabel.willy@gmail.com>
2025-01-30 11:16:46 +0100

    contact change

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -3,6 +3,7 @@ import validator from "validator";
 import "./Contact.css";
 import paperPlane from "../../assets/paper.svg";
 import { API_URL } from "../../config/api";
+
 /**
  * Composant Contact pour la soumission d'un formulaire.
  *
@@ -21,6 +22,7 @@ const Contact = () => {
     message: "",
   });
   const [messageSent, setMessageSent] = useState(false);
+  const [error, setError] = useState(null);
 
   /**
    * Gestionnaire de changement pour le formulaire.
@@ -37,63 +39,70 @@ const Contact = () => {
    * Gestionnaire de soumission du formulaire.
    * @param {Event} e - L'événement de soumission.
    */
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    //--------- Nettoyage et Validation des entrées------------
-    // Nettoyage des données du formulaire
+    // Nettoyage et validation des entrées
     const cleanedName = validator.escape(formData.name);
     const cleanedEmail = validator.normalizeEmail(formData.email);
     const cleanedMessage = validator.escape(formData.message);
+
     // Vérification de l'adresse e-mail
     const emailRegex = /^[a-z]+@[a-z]+\.[a-z]+$/;
     if (!emailRegex.test(cleanedEmail)) {
-      alert("Veuillez entrer une adresse e-mail valide.");
+      setError("Veuillez entrer une adresse e-mail valide.");
       return;
     }
+
     // Vérification du nom
     const nameRegex = /^[a-zA-Z\s]+$/;
     if (!nameRegex.test(cleanedName)) {
-      alert("Nom invalide. Seules les lettres et les espaces sont autorisés.");
+      setError(
+        "Nom invalide. Seules les lettres et les espaces sont autorisés."
+      );
       return;
     }
+
     // Vérification du message
     const messageRegex = /^[a-zA-Z0-9\s.,!?'"-]*$/;
     if (!messageRegex.test(cleanedMessage)) {
-      alert(
+      setError(
         "Message invalide. Seules les lettres, chiffres et certains caractères spéciaux sont autorisés."
       );
       return;
     }
-    //---------------------------------------------------------
 
-    // Envoi de l'email vers la route send-email
-    fetch(`${API_URL}/send-email`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: cleanedName,
-        email: cleanedEmail,
-        message: cleanedMessage,
-      }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log("Email envoyé avec succès !");
-          setFormData({ name: "", email: "", message: "" });
-          setMessageSent(true);
-          setTimeout(() => {
-            setMessageSent(false);
-          }, 2500);
-        } else {
-          throw new Error("Échec de l'envoi de l'email.");
-        }
-      })
-      .catch((error) => {
-        console.error(error);
+    try {
+      const response = await fetch(`${API_URL}/send-email`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: cleanedName,
+          email: cleanedEmail,
+          message: cleanedMessage,
+        }),
       });
+
+      if (!response.ok) {
+        throw new Error(`erreur HTTP status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Email envoyé avec succès !");
+      setFormData({ name: "", email: "", message: "" });
+      setMessageSent(true);
+      setError(null);
+      setTimeout(() => {
+        setMessageSent(false);
+      }, 2500);
+    } catch (error) {
+      console.error("Erreur lors de l'envoi du formulaire:", error);
+      setError(
+        "Une erreur est survenue lors de l'envoi du message. Veuillez réessayer."
+      );
+    }
   };
 
   return (
@@ -103,6 +112,7 @@ const Contact = () => {
       <div className={`message-sent ${messageSent ? "show" : ""}`}>
         {messageSent && <p>Message envoyé avec succès !</p>}
       </div>
+      {error && <p className="error-message">{error}</p>}
       <div className="contact-form">
         <form onSubmit={handleSubmit}>
           <div className="form-group">
